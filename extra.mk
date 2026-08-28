a5b47b3ef6dee8c68d25c7e26c00bfafaa84ab0e
Aditya Eka Pranata <aepranata@gmail.com>
2026-08-28 23:42:38 +0700

    extra: Inherit some package from gms
    Change-Id: I6f66b7c3e53330a9db5fe19a5e536fd6795e094d

--- extra.mk
+++ extra.mk
@@ -8,3 +8,22 @@ PRODUCT_PACKAGES += \
 # Gmail
 PRODUCT_PACKAGES += \
     PrebuiltGmail
+
+# From Gms
+TARGET_INCLUDE_EXTRA_APPS := false
+
+PRODUCT_COPY_FILES += \
+    vendor/gms/prebuilts/proprietary/product/app/Chrome/Chrome.apk.gz:$(TARGET_COPY_OUT_PRODUCT)/app/Chrome/Chrome.apk.gz \
+    vendor/gms/prebuilts/proprietary/product/app/TrichromeLibrary/TrichromeLibrary.apk.gz:$(TARGET_COPY_OUT_PRODUCT)/app/TrichromeLibrary/TrichromeLibrary.apk.gz \
+    vendor/gms/prebuilts/proprietary/product/app/WebViewGoogle/WebViewGoogle.apk.gz:$(TARGET_COPY_OUT_PRODUCT)/app/WebViewGoogle/WebViewGoogle.apk.gz
+
+PRODUCT_PACKAGES += \
+    AccessibilityMenu \
+    AmbientStreaming \
+    Chrome-Stub \
+    LocationHistoryPrebuilt \
+    Photos \
+    talkback \
+    TrichromeLibrary-Stub \
+    WebViewGoogle-Stub \
+    Velvet
